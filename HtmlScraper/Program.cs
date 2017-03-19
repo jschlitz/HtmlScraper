c7b3c306494889f2ae669b1f691f72be785694c3
Jacob Schlitz <jschlitz@gmail.com>
2017-03-19 19:37:37 -0400

    download pages

--- HtmlScraper/Program.cs
+++ HtmlScraper/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace HtmlScraper
 {
@@ -10,6 +11,37 @@ namespace HtmlScraper
   {
     static void Main(string[] args)
     {
+
+      using (var client = new System.Net.WebClient())
+      {
+        var baseUrl = args[0];
+        var query = args[1];
+        foreach (var item in GetPostPages(client, baseUrl, query))
+        {
+          Console.WriteLine(item);
+        }
+        ;
+
+        Console.ReadKey(true);
+
+      }
+    }
+
+    private static string[] GetPostPages(System.Net.WebClient client, string baseUrl, string query)
+    {
+      using(var stream = client.OpenRead(baseUrl + @"/posts?tags=" + query))
+      {
+        var doc = new HtmlDocument();
+        doc.Load(stream);
+        var descendents = doc.DocumentNode.Descendants().Where(h => h.Attributes.Contains("Class") && h.Attributes["Class"].Value == "post-preview").ToList();
+        var articles = doc.DocumentNode.SelectNodes(@"//article[@class]")
+          .Where(hn => hn.Attributes["class"].Value.Split(' ').Contains("post-preview"))
+          .ToList();
+        var hrefs = articles.Select(hn => hn.Element("a").Attributes["href"])
+          .Select(s=>baseUrl + s.Value)
+          .ToArray();
+        return hrefs;
+      }
     }
   }
 }
