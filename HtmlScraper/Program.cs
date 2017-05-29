10ef30c2d7dbf1642b9048ba83cd308f181b5c2a
Jacob Schlitz <jschlitz@gmail.com>
2017-05-28 20:18:03 -0400

    grabbing images.

--- HtmlScraper/Program.cs
+++ HtmlScraper/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using HtmlAgilityPack;
+using System.Threading;
 
 namespace HtmlScraper
 {
@@ -16,32 +19,97 @@ namespace HtmlScraper
       {
         var baseUrl = args[0];
         var query = args[1];
-        foreach (var item in GetPostPages(client, baseUrl, query))
+
+        var targetDir = args.Length >=3 ? args[2] : System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+        if (!Directory.Exists(targetDir)) throw new Exception($"Target directory does not exist: \r{targetDir}");
+        targetDir = Path.Combine(targetDir, query);
+        Directory.CreateDirectory(targetDir);
+
+
+        var pages = new Dictionary<string, bool>();
+        var posts = new Dictionary<string, bool>();
+        pages[CombineUri(baseUrl, @"/posts?tags=" + query)] = false;
+
+        while (pages.Any(kvp=>!kvp.Value))
         {
-          Console.WriteLine(item);
+          var queryUri = pages.First(kvp => !kvp.Value).Key;
+
+          using (var stream = client.OpenRead(queryUri))
+          {
+            var doc = new HtmlDocument();
+            doc.Load(stream);
+
+            Console.WriteLine("-------- pages --------");
+            foreach (var item in GetPagination(doc, baseUrl))
+            {
+              if (!pages.ContainsKey(item)) pages[item] = false; 
+            }
+
+            Console.WriteLine("-------- posts --------");
+            foreach (var item in GetPostPages(doc, baseUrl, query))
+            {
+              if (!posts.ContainsKey(item)) posts[item] = false;
+            };
+          }
+
+          pages[queryUri] = true;
         }
-        ;
+
+
+        VisitPosts(posts, client, baseUrl, targetDir);
 
         Console.ReadKey(true);
 
       }
     }
 
-    private static string[] GetPostPages(System.Net.WebClient client, string baseUrl, string query)
+    private static void VisitPosts(Dictionary<string, bool> posts, System.Net.WebClient client, string baseUrl, string targetDir)
     {
-      using(var stream = client.OpenRead(baseUrl + @"/posts?tags=" + query))
+      foreach (var item in posts.Where(kvp => !kvp.Value).Select(kvp => kvp.Key).ToArray())
       {
-        var doc = new HtmlDocument();
-        doc.Load(stream);
-        var descendents = doc.DocumentNode.Descendants().Where(h => h.Attributes.Contains("Class") && h.Attributes["Class"].Value == "post-preview").ToList();
-        var articles = doc.DocumentNode.SelectNodes(@"//article[@class]")
-          .Where(hn => hn.Attributes["class"].Value.Split(' ').Contains("post-preview"))
-          .ToList();
-        var hrefs = articles.Select(hn => hn.Element("a").Attributes["href"])
-          .Select(s=>baseUrl + s.Value)
-          .ToArray();
-        return hrefs;
+        using (var stream = client.OpenRead(item))
+        {
+          var doc = new HtmlDocument();
+          doc.Load(stream);
+
+          var n = doc.DocumentNode.SelectSingleNode("//img[@id='image']");
+          var target = n.GetAttributeValue("src", "");
+          if (string.IsNullOrEmpty(target)) continue;
+          target = CombineUri(baseUrl, target);
+          //client.DownloadFile(target, Path.Combine(targetDir, Path.GetFileName(target).Trim('_')));
+          Console.WriteLine(Path.Combine(targetDir, Path.GetFileName(target).Trim('_')));
+          posts[item] = true;//visited
+
+          Thread.Sleep(222);
+        }
       }
+    }
+
+    private static string[] GetPagination(HtmlDocument doc, string baseUrl)
+    {
+      var links = doc.DocumentNode.SelectNodes("//div[@class='paginator']/menu/li/a");
+      return links
+        .Select(hn => CombineUri(baseUrl, HttpUtility.HtmlDecode(hn.Attributes["href"].Value)))
+        .ToArray();
+    }
+
+    public static string CombineUri(string uri1, string uri2)
+    {
+      uri1 = uri1.TrimEnd('/');
+      uri2 = uri2.TrimStart('/');
+      return string.Format("{0}/{1}", uri1, uri2);
+    }
+
+    private static string[] GetPostPages(HtmlDocument doc,  string baseUrl, string query)
+    {
+      var descendents = doc.DocumentNode.Descendants().Where(h => h.Attributes.Contains("Class") && h.Attributes["Class"].Value == "post-preview").ToList();
+      var articles = doc.DocumentNode.SelectNodes(@"//article[@class]")
+        .Where(hn => hn.Attributes["class"].Value.Split(' ').Contains("post-preview"))
+        .ToList();
+      var hrefs = articles.Select(hn => hn.Element("a").Attributes["href"])
+        .Select(s => CombineUri(baseUrl, HttpUtility.HtmlDecode(s.Value)))
+        .ToArray();
+      return hrefs;
     }
   }
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HtmlScraper
 Let's automate this
+
+remember: http://ricostacruz.com/cheatsheets/xpath.html#class-check
+https://github.com/darrylwhitmore/NScrape
